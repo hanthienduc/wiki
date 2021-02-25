b616527be1463c32b08519024af560c35d219e6c
hanthienduc <hanthienduc.96@gmail.com>
2021-02-25 14:04:27 +0900

    finish create entry page, add basic layout for edit page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,6 +7,8 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("search", views.search, name="search"),
+    path("edit_page", views.edit_page, name="edit_page"),
+    path("create_page", views.create_page, name="create_page"),
     path("<str:name>", views.entry, name="entry"),
     # path(r'^wiki/$', views.entry, name="entry"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django import forms
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from . import util
+from html.parser import HTMLParser
+
+
+class HTMLFilter(HTMLParser):
+    text = ""
+
+    def handle_data(self, data):
+        self.text += data
 
 
 class searchForm(forms.Form):
@@ -24,7 +32,9 @@ def index(request):
 def entry(request, name):
 
     if not util.get_entry(name):
-        return render(request, "encyclopedia/error.html")
+        return render(request, "encyclopedia/error.html", {
+            "title": "Not found"
+        })
     else:
         return render(request, "encyclopedia/entry.html", {
             "title": name.upper(),
@@ -60,5 +70,83 @@ def search(request):
                             "title": search,
                             "entries": request.session["subsearch"]
                         })
-                    
-            return HttpResponseRedirect(reverse("encyclopedia:index"))     
+
+            return HttpResponseRedirect(reverse("encyclopedia:index"))
+
+
+class NewPageForm(forms.Form):
+    title = forms.CharField(label="title")
+    content = forms.CharField(
+        label="content", widget=forms.Textarea)
+
+    def editForm(self, title, content):
+        self.title = forms.CharField(label="title", initial={title})
+        self.content = forms.CharField(
+            label="content", widget=forms.Textarea, initial={content})
+
+
+def create_page(request):
+    if request.method == "POST":
+
+        form = NewPageForm(request.POST)
+
+        if form.is_valid():
+
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            checkEntry = util.get_entry(title)
+            if not checkEntry:
+
+                util.save_entry(title, content)
+                return render(request, "encyclopedia/entry.html", {
+                    "title": title,
+                    "entry": util.get_entry(title)
+                })
+            else:
+                return render(request, "encyclopedia/create.html", {
+                    "title_page": title + " already included",
+                    "showbutton": False
+                })
+        else:
+            return render(request, "encyclopedia/error.html", {
+                "title": "Form invalid"
+            })
+
+    return render(request, "encyclopedia/create.html", {
+        "title_page": "Create Page",
+        "showbutton": True,
+        "pageform": NewPageForm()
+    })
+
+
+class EditForm(forms.Form):
+    title = forms.CharField(label="title", initial={title})
+    content = forms.CharField(
+        label="content", widget=forms.Textarea, initial={content})
+
+
+def edit_page(request):
+
+    if(request.method == "POST"):
+
+        if 'title' and 'content' in request.POST:
+            title = 'You editing for: %r' % request.POST['title']
+            f = HTMLFilter()
+            f.feed(request.POST['content'])
+            content = f.text
+            return render(request, "encyclopedia/edit.html", {
+                "formedit": EditForm(title, content)
+            }
+            )
+        else:
+            title = 'You submitted an empty form.'
+
+    else:
+
+        return HttpResponse("Invalid data")
+
+
+    return render(request, "encyclopedia/edit.html", {
+        "formedit": EditForm()
+    }
+    )
